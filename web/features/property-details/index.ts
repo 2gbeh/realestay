6a3f05d3084ec05ee29e0f7187b681c61c434279
etugbeh-plateaumed <etugbeh@plateaumed.com>
2025-02-16 21:23:20 +0100

    feat(web): add property CheckoutCard

--- web/features/property-details/index.ts
+++ web/features/property-details/index.ts
@@ -1,5 +1,6 @@
 export { default as AgentProfileCard } from "./components/AgentProfileCard";
 export { default as AgentProfileDetails } from "./components/AgentProfileDetails";
+export { default as CheckoutCard } from "./components/CheckoutCard";
 export { default as Policy } from "./components/Policy";
 export { default as PropertyMap } from "./components/PropertyMap";
 export { default as Ratings } from "./components/Ratings";
--- web/pages/property/[...slug].tsx
+++ web/pages/property/[...slug].tsx
@@ -22,6 +22,7 @@ import {
   Policy,
   Reviews,
   Ratings,
+  CheckoutCard,
 } from "@/features/property-details";
 
 const PropertyDetails: NextPageWithLayout = () => {
@@ -151,51 +152,7 @@ const PropertyDetails: NextPageWithLayout = () => {
                 <button className="btnOutline">Show all 33 amenities</button>
               </div>
             </section>
-            <aside className="flex-1 self-start rounded-lg border px-8 py-5 shadow-xl">
-              <div className="flexEnd gap-1">
-                <h2 className="text-2xl font-medium">$87</h2>
-                <p>night</p>
-              </div>
-              <div className="flexCenter mt-5">
-                <div className="flexCol flex-1 rounded-tl-xl border px-4 py-2 text-sm">
-                  <strong className="">CHECK-IN</strong>
-                  <small>3/5/2025</small>
-                </div>
-                <div className="flexCol flex-1 rounded-tr-xl border px-4 py-2 text-sm">
-                  <strong className="">CHECKOUT</strong>
-                  <small>3/10/2025</small>
-                </div>
-              </div>
-              <div className="flexCenterBetween rounded-bl-xl rounded-br-xl border border-t-0 px-4 py-2">
-                <div className="flexCol">
-                  <strong className="text-sm">GUEST</strong>
-                  <small>1 guest</small>
-                </div>
-                <LuChevronDown size={20} />
-              </div>
-              <button className="btn mt-5 w-full rounded-full">Reserve</button>
-              <p className="text-sm_ mutedText mt-4 text-center">
-                You won't be charged yet
-              </p>
-              <ul className="mt-5 space-y-4">
-                <li className="flexCenterBetween">
-                  <span className="underline">$87 x 5 nights</span>
-                  <span>$437</span>
-                </li>
-                <li className="flexCenterBetween">
-                  <span className="underline">Cleaning fee</span>
-                  <span>$53</span>
-                </li>
-                <li className="flexCenterBetween">
-                  <span className="underline">Airbnb service fee</span>
-                  <span>$80</span>
-                </li>
-              </ul>
-              <div className="flexCenterBetween mt-5 border-t py-5 font-medium">
-                <span>Total before taxes</span>
-                <span>$570</span>
-              </div>
-            </aside>
+            <CheckoutCard />
           </div>
           {/* RATINGS */}
           <div className="border-t"></div>
